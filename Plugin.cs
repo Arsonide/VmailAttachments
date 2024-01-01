21c1470439b10ed93f692c8c572224584f04dcc0
Arsonide <arsonide@yahoo.com>
2024-01-01 10:33:06 -0600

    Pushing the initial project files.

---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using SOD.Common.BepInEx;
+
+namespace vmail_attachments;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : PluginController<Plugin>
+{
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Harmony harmony = new Harmony($"{MyPluginInfo.PLUGIN_GUID}");
+        harmony.PatchAll();
+    }
+}
